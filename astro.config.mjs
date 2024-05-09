a05d01cb3f323963c488f7a74c82dd40fda6418d
hslee <redlasha@gmail.com>
2024-05-10 02:33:15 +0900

    chore: setting deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://redlasha.github.io',
+  base: 'astro-exam-01',
   integrations: [react(), vue()]
 });
--- src/scripts/about2.js
+++
@@ -1,3 +0,0 @@
-let p = document.createElement('p')
-p.textContent = 'about page content ... '
-document.body.appendChild(p)
